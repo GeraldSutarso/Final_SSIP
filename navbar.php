8b68c37e247f0cb4e65d34496c92c67d6fcf603c
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2022-12-15 18:24:21 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -13,11 +13,11 @@
       <li><a href="#">Page 4</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false) {?>
-      <li><a href="register_form.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="login_form.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    <?php } else if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){?>
-        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {?>
+    <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    <?php } else if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false){?>
+        <li><a href="register_form.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login_form.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     <?php }?>
     </ul>
   </div>
