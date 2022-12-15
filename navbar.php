41f40884ebca90b86d5ae5178329cb7089027f8c
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2022-12-15 15:30:02 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -13,8 +13,12 @@
       <li><a href="#">Page 4</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false) {?>
+      <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    <?php } else {?>
+        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    <?php }?>
     </ul>
   </div>
 </nav>
