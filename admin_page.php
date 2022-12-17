80a379af52f504cc662328d88712dc60e55add9e
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2022-12-17 09:10:47 +0700

    adminpage

--- admin_page.php
+++ admin_page.php
@@ -10,29 +10,42 @@ if(!isset($_SESSION['admin_name'])){
 <html lang="en">
 <head>
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Admin Page</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+   <title>Rental Mobil | Admin Dashboard</title>
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
+<?php include('header.php');?>
    
-<div class="container">
+<div class="ts-main-content">
+<?php include('leftbar.php');?>
+<div class="content-wrapper">
+			<div class="container-fluid">
 
-<div class="content">
-      <h3>hi, <span>admin</span></h3>
-      <a href="index.php">Home</a>
-      <h1>Welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
-      <p>This is an admin page</p>
-      <a href="login_form.php" class="btn">Login</a>
-      <a href="register_form.php" class="btn">Register</a>
-      <a href="logout.php" class="btn">Logout</a>
-   </div>
-
+			</div>                          
 </div>
+
 
 </body>
 </html>
