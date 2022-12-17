8621f1ef9240f288ed116aaff712faef7f602603
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2022-12-17 12:25:10 +0700

    Update user_page.php

--- footer.php
+++ footer.php
@@ -1,77 +1,20 @@
 <!-- Remove the container if you want to extend the Footer to full width. -->
 
 
-  <footer class="footer fixed-bottom" style="background-color: #ffede7;">
+  <footer class="footer fixed-bottom" style="background-color: #85C1E9;">
     <div class="container p-4">
       <div class="row">
         <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">About us</h5>
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #ffff;">About us</h5>
           <ul class="list-unstyled mb-0">
             <li class="mb-1">
               <a href="#!" style="color: #4f4f4f;">FAQs</a>
             </li>
             <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
+              <a href="#!" style="color: #4f4f4f;">Contact Us</a>
+            </li>          
           </ul>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">Lorem ipsum</h5>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">Lorem ipsum</h5>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4">
-          <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">Lorem ipsum</h5>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li class="mb-1">
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#!" style="color: #4f4f4f;">Lorem ipsum</a>
-            </li>
-          </ul>
-        </div>
+        </div>             
       </div>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
--- user_page.php
+++ user_page.php
@@ -19,6 +19,51 @@ if(!isset($_SESSION['user_name'])){
     <body>
     <?php 
     include('navbar.php'); ?>
+        <div id="sec2" style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+        <h3 style="text-align:center;">Available Cars</h3>
+<br>
+        <section class="menu-content">
+            <?php   
+            $sql1 = "SELECT * FROM cars WHERE car_availability='yes'";
+            $result1 = mysqli_query($conn,$sql1);
+
+            if(mysqli_num_rows($result1) > 0) {
+                while($row1 = mysqli_fetch_assoc($result1)){
+                    $car_id = $row1["car_id"];
+                    $car_name = $row1["car_name"];
+                    $price = $row1["price"];
+                    $year = $row1["year"];
+                    $car_img = $row1["car_img"];
+               
+                    ?>
+            <a href="booking.php?id=<?php echo($car_id) ?>">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="<?php echo $car_img; ?>" alt="Card image cap">
+            <h5><b> <?php echo $car_name; ?> </b></h5>
+            <h6> Price: <?php echo ("Rp. " . $price . "/km "); ?></h6>
+            <h6> Year: <?php echo ($year); ?></h6>
+
+            
+            </div> 
+            </a>
+            <?php }}
+            else {
+                ?>
+<h1> No cars available :( </h1>
+                <?php
+            }
+            ?>                                   
+        </section>
+                    
+    </div>
+
+    <div class="bgimg-2">
+        <div class="caption">
+            <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;"></span>
+        </div>
+    </div>
     <h2></h2>
     <?php include('footer.php');?>
     </body>
