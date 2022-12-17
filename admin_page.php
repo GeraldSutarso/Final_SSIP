fe8b837c80ae51cb3862d9aadcf8630c3042d574
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2022-12-17 09:31:20 +0700

    adminpage

--- admin_page.php
+++ admin_page.php
@@ -19,18 +19,6 @@ if(!isset($_SESSION['admin_name'])){
    <script src="https://kit.fontawesome.com/dab3ca4542.js" crossorigin="anonymous"></script>
    <!-- Font awesome -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<!-- Sandstone Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- Bootstrap Datatables -->
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<!-- Bootstrap social button library -->
-	<link rel="stylesheet" href="css/bootstrap-social.css">
-	<!-- Bootstrap select -->
-	<link rel="stylesheet" href="css/bootstrap-select.css">
-	<!-- Bootstrap file input -->
-	<link rel="stylesheet" href="css/fileinput.min.css">
-	<!-- Awesome Bootstrap checkbox -->
-	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
 	<!-- Admin Stye -->
 	<link rel="stylesheet" href="css/style.css">
 
