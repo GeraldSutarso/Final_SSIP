06e215e8ace1f89be04bb82b9726fe22219556ca
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2022-12-18 14:18:55 +0700

    Update rent.sql

--- rent.sql
+++ rent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2022 at 09:33 AM
+-- Generation Time: Dec 18, 2022 at 08:18 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,17 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `book_id` int(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `car_id` int(50) NOT NULL,
+  `driver_id` varchar(15) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `price` float NOT NULL,
+  `no_days` int(50) DEFAULT NULL,
+  `total_price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cars`
 --
 
 CREATE TABLE `cars` (
-  `car_id` int(255) NOT NULL,
+  `car_id` int(225) NOT NULL,
   `car_name` varchar(50) NOT NULL,
   `car_nameplate` varchar(50) NOT NULL,
   `car_img` varchar(50) DEFAULT 'NA',
   `price` int(11) DEFAULT NULL,
   `year` int(6) DEFAULT NULL,
-  `car_availability` tinyint(1) DEFAULT 1
+  `car_availability` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,7 +60,8 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`car_id`, `car_name`, `car_nameplate`, `car_img`, `price`, `year`, `car_availability`) VALUES
-(1, 'Audi A4', 'GA3KA6969', 'assets/img/cars/audi-a4.jpg', 10000, 2012, 1);
+(13, 'Toyota Fortuner', 'B 15 GTR', 'Fortuner.png', 700000, 2016, '1'),
+(14, 'Honda CRV', 'B 1434 RYT', 'hondacr.jpg', 1000000, 2021, '1');
 
 -- --------------------------------------------------------
 
@@ -51,13 +70,13 @@ INSERT INTO `cars` (`car_id`, `car_name`, `car_nameplate`, `car_img`, `price`, `
 --
 
 CREATE TABLE `driver` (
-  `driver_id` int(255) NOT NULL,
+  `driver_id` int(20) NOT NULL,
   `driver_name` varchar(50) NOT NULL,
   `dl_number` varchar(50) NOT NULL,
   `driver_phone` varchar(15) NOT NULL,
   `driver_address` varchar(50) NOT NULL,
   `driver_gender` varchar(10) NOT NULL,
-  `driver_availability` tinyint(1) DEFAULT 1
+  `driver_availability` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -65,8 +84,30 @@ CREATE TABLE `driver` (
 --
 
 INSERT INTO `driver` (`driver_id`, `driver_name`, `dl_number`, `driver_phone`, `driver_address`, `driver_gender`, `driver_availability`) VALUES
-(2, 'Gerald', '1231231', '+61231231', 'Jl. ASDS no. 321', 'Male', 1),
-(3, 'Dlareg', '1231232', '+61231213', 'Jl. ASDS no. 322', 'Female', 1);
+(6, 'Jhon', '01010101', '0856335267', 'Cikarang', 'Male', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rentedcars`
+--
+
+CREATE TABLE `rentedcars` (
+  `id` int(100) NOT NULL,
+  `customer_username` varchar(50) NOT NULL,
+  `car_id` int(20) NOT NULL,
+  `driver_id` int(20) NOT NULL,
+  `booking_date` date NOT NULL,
+  `rent_start_date` date NOT NULL,
+  `rent_end_date` date NOT NULL,
+  `car_return_date` date DEFAULT NULL,
+  `fare` double NOT NULL,
+  `charge_type` varchar(25) NOT NULL DEFAULT 'days',
+  `distance` double DEFAULT NULL,
+  `no_of_days` int(50) DEFAULT NULL,
+  `total_amount` double DEFAULT NULL,
+  `return_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,12 +130,17 @@ CREATE TABLE `user_form` (
 INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'ARZIDAN ', 'arzidanakbar7@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'user'),
 (2, 'Geraldadmin', 'testTwo@mail.co.id', 'e00cf25ad42683b3df678c61f42c6bda', 'admin'),
-(3, 'test', 'test@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'admin'),
-(4, 'Geralduser', 'geralduser@mail.com', '24c9e15e52afc47c225b757e7bee1f9d', 'user');
+(3, 'test', 'test@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`car_id`);
 
 --
 -- Indexes for table `cars`
@@ -119,22 +165,28 @@ ALTER TABLE `user_form`
 --
 
 --
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `car_id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `car_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `car_id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `driver`
 --
 ALTER TABLE `driver`
-  MODIFY `driver_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `driver_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_form`
 --
 ALTER TABLE `user_form`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
