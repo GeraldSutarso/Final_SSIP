f5b2ffcfb7ea8302ba464d50432d74995318ba99
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2022-12-18 14:36:44 +0700

    Update rent.sql

--- rent.sql
+++ rent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2022 at 08:18 AM
+-- Generation Time: Dec 18, 2022 at 08:35 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking` (
   `book_id` int(255) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `user_id` int(50) NOT NULL,
   `car_id` int(50) NOT NULL,
-  `driver_id` varchar(15) NOT NULL,
+  `driver_id` int(50) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   `price` float NOT NULL,
@@ -46,13 +46,13 @@ CREATE TABLE `booking` (
 --
 
 CREATE TABLE `cars` (
-  `car_id` int(225) NOT NULL,
+  `car_id` int(255) NOT NULL,
   `car_name` varchar(50) NOT NULL,
   `car_nameplate` varchar(50) NOT NULL,
   `car_img` varchar(50) DEFAULT 'NA',
   `price` int(11) DEFAULT NULL,
   `year` int(6) DEFAULT NULL,
-  `car_availability` varchar(10) NOT NULL
+  `car_availability` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -60,8 +60,11 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`car_id`, `car_name`, `car_nameplate`, `car_img`, `price`, `year`, `car_availability`) VALUES
-(13, 'Toyota Fortuner', 'B 15 GTR', 'Fortuner.png', 700000, 2016, '1'),
-(14, 'Honda CRV', 'B 1434 RYT', 'hondacr.jpg', 1000000, 2021, '1');
+(3, 'Hyundai', 'B313A312', 'hyundai0.png', 200000, 2003, 1),
+(4, 'McQueen', '95 RUSTEZE', 'mcqueen.png', 150000, 2006, 1),
+(7, 'Jeep', 'M1213HUH', 'jeep.jpg', 230000, 2005, 1),
+(8, 'Innova', 'J9999JJJ', 'innova.png', 999999, 2022, 1),
+(10, 'Veloz', 'G6763NUB', 'veloz.png', 500000, 2000, 1);
 
 -- --------------------------------------------------------
 
@@ -70,13 +73,13 @@ INSERT INTO `cars` (`car_id`, `car_name`, `car_nameplate`, `car_img`, `price`, `
 --
 
 CREATE TABLE `driver` (
-  `driver_id` int(20) NOT NULL,
+  `driver_id` int(255) NOT NULL,
   `driver_name` varchar(50) NOT NULL,
   `dl_number` varchar(50) NOT NULL,
   `driver_phone` varchar(15) NOT NULL,
   `driver_address` varchar(50) NOT NULL,
   `driver_gender` varchar(10) NOT NULL,
-  `driver_availability` varchar(10) NOT NULL
+  `driver_availability` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -84,30 +87,9 @@ CREATE TABLE `driver` (
 --
 
 INSERT INTO `driver` (`driver_id`, `driver_name`, `dl_number`, `driver_phone`, `driver_address`, `driver_gender`, `driver_availability`) VALUES
-(6, 'Jhon', '01010101', '0856335267', 'Cikarang', 'Male', '1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rentedcars`
---
-
-CREATE TABLE `rentedcars` (
-  `id` int(100) NOT NULL,
-  `customer_username` varchar(50) NOT NULL,
-  `car_id` int(20) NOT NULL,
-  `driver_id` int(20) NOT NULL,
-  `booking_date` date NOT NULL,
-  `rent_start_date` date NOT NULL,
-  `rent_end_date` date NOT NULL,
-  `car_return_date` date DEFAULT NULL,
-  `fare` double NOT NULL,
-  `charge_type` varchar(25) NOT NULL DEFAULT 'days',
-  `distance` double DEFAULT NULL,
-  `no_of_days` int(50) DEFAULT NULL,
-  `total_amount` double DEFAULT NULL,
-  `return_status` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(2, 'Gerald', '1231231', '+61231231', 'Jl.Beringin', 'Male', 1),
+(3, 'Dlareg', '1231232', '+61231213', 'Jln.Sukabumi', 'Female', 1),
+(4, 'Budi', '76712391', '+628777813122', 'jln.Bukitrendah', 'Male', 1);
 
 -- --------------------------------------------------------
 
@@ -130,7 +112,8 @@ CREATE TABLE `user_form` (
 INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'ARZIDAN ', 'arzidanakbar7@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'user'),
 (2, 'Geraldadmin', 'testTwo@mail.co.id', 'e00cf25ad42683b3df678c61f42c6bda', 'admin'),
-(3, 'test', 'test@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'admin');
+(3, 'test', 'test@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', 'admin'),
+(4, 'Geralduser', 'geralduser@mail.com', '24c9e15e52afc47c225b757e7bee1f9d', 'user');
 
 --
 -- Indexes for dumped tables
@@ -140,7 +123,10 @@ INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`car_id`);
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `FK_PersonOrder` (`user_id`),
+  ADD KEY `car_id` (`car_id`),
+  ADD KEY `driver_id` (`driver_id`);
 
 --
 -- Indexes for table `cars`
@@ -165,28 +151,33 @@ ALTER TABLE `user_form`
 --
 
 --
--- AUTO_INCREMENT for table `booking`
---
-ALTER TABLE `booking`
-  MODIFY `car_id` int(50) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `car_id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `car_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `driver`
 --
 ALTER TABLE `driver`
-  MODIFY `driver_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `driver_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_form`
 --
 ALTER TABLE `user_form`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
