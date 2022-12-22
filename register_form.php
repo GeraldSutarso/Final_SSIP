8b86e01cedf1d7e0db4b8a012701fcb5285a4df8
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2022-12-23 01:26:58 +0700

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -56,10 +56,6 @@ if(isset($_POST['submit'])){
    <input type="email" name="email" required placeholder="enter your email">
    <input type="password" name="password" required placeholder="enter your password">
    <input type="password" name="cpassword" required placeholder="confirm your password">
-   <select name="user_type">
-      <option value="user">user</option>
-      <option value="admin">admin</option>
-   </select>
    <input type="submit" name="submit" value="register now" class="form-btn">
    <p>already have an account? <a href="login_form.php">login now</a></p>
 </form>
